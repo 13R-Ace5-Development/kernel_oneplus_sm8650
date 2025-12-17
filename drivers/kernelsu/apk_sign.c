fb97be129f6e3fcfe6c7a0a1586c2fbc353cc764
Pranav Vashi <neobuddy89@gmail.com>
2025-12-18 00:02:45 +0530

    drivers: Add hash for ksu-next
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/kernelsu/apk_sign.c
+++ drivers/kernelsu/apk_sign.c
@@ -312,6 +312,7 @@ bool is_manager_apk(char *path)
     return check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH);
 #else
     return (check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH) ||
-            check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4")); // 5ec1cff
+            check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4") || // 5ec1cff
+            check_v2_signature(path, 0x3e6, "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7")); // KSU-Next
 #endif // #ifndef CONFIG_KSU_SUSFS
 }
